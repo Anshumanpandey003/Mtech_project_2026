67ef8be4e0b27b8ba70c6386ce782a85c0e750e5
Anshumanpandey003 <160728292+Anshumanpandey003@users.noreply.github.com>
2026-03-07 03:24:36 +0530

    Create App component with GrievanceSystem

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GrievanceSystem from './components/GrievanceSystem';
+
+function App() {
+  return (
+    <div className="App">
+      <GrievanceSystem />
+    </div>
+  );
+}
+
+export default App;
